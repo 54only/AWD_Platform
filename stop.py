fa3bded5f1737649eca4ab669e6af12c4085502a
54only <249012608@qq.com>
2019-07-31 04:30:08 -0700

    edit sleep time

---
+++ stop.py
@@ -0,0 +1,19 @@
+from dockercontr import clear_container
+from models import db,Teams
+
+
+
+def main():
+    teams = Teams.query.all()
+    for i in teams:
+        clear_container(i.teamcontainer)    
+        print(i.teamcontainer,'removed')
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+    print('''# docker container prune
+    # docker volume rm $(docker volume ls -qf dangling=true)''')
