75e950d92f21395b2d11a509827c361c71bc8ec2
54only <42793459+54only@users.noreply.github.com>
2020-01-24 21:44:46 +0800

    Update start.py

--- start.py
+++ start.py
@@ -36,9 +36,9 @@ timespan = 1 * 60 # 刷新 flag 时间
 '''
 # port 规则为 3 00 队伍id 22 服务端口
 subject =   {
-            #'yunnam_simple': {'sshport':30022,'serviceport':30080},
-            #'pwn_simple':   {'sshport':30032,'serviceport':30090},
-            'tomcat8':  {'sshport':30042,'serviceport':30040},
+            'yunnam_simple': {'sshport':30022,'serviceport':30080},
+            'pwn_simple':   {'sshport':30032,'serviceport':30090},
+            #'tomcat8':  {'sshport':30042,'serviceport':30040},
             }
 
 #清除 docker 残留数据
